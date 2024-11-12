682b54b786dd176c0456247d7d06461187e8dd40
AlexBB22 <41007236+AlexBB22@users.noreply.github.com>
2024-11-12 17:29:31 +0100

    fix: update to greeting for blue

--- src/main/java/echoMsg.java
+++ src/main/java/echoMsg.java
@@ -4,10 +4,10 @@ public class echoMsg  {
     public static void main(String[] args) {
       String value = System.getenv("USERNAME");
       if (value != null) {
-          System.out.format("Greetings %s!\n",value);
-      } else {
-          System.out.println("Greetings!");
-      }
+         System.out.format("Greetings blue %s!\n",value);
+         } else {
+         System.out.println("Greetings blue!");
+         }
 
       if (args.length > 0) {
          System.out.println("Here are the arguments you passed in.");
